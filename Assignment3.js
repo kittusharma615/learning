dbd5aae25b6e5e5fc39c664502a8b6c1da7575d6
Kavita <kittu.sharma615@gmail.com>
2025-03-31 09:08:44 +0530

    assginment 3 WIP

--- Assignment3.js
+++ Assignment3.js
@@ -1,16 +1,22 @@
-// const createPassword=(n)=>{
-//         const str='qwertyuiopasdfghjklzxcvbnm,QWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^'
-//     let bag='';
-//         for(i=0;i<n;i++ ){
-//     const randomIndex=Math.floor(Math.random()*str.length)
-//     bag+=str[randomIndex];
-//         }
-//         return bag
-    
-//     }
-    
-//     setInterval(()=>{
-//         console.log(createPassword(5))
-//     },1000)
+const createPassword = (n) => {
+    const str = 'qwertyuiopasdfghjklzxcvbnm,QWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^'
+    let bag = '';
+    for (i = 0; i < n; i++) {
+        const randomIndex = Math.floor(Math.random() * str.length)
+        bag += str[randomIndex];
+    }
+    return bag
 
-    const 
+}
+
+// setInterval(() => {
+//     console.log(createPassword(5))
+// }, 1000)
+
+const countAlphabets = () => {
+    let a = 0;
+    let b = 1;
+    let z = 36;
+    for (i = a; i <= z; i++) {
+    }
+}
